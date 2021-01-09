eea23a55d34ee03e6629aa513a127cfb389c25e2
Jakub Twardosz <jtwardosz13@gmail.com>
2021-01-10 00:37:36 +0100

    Task 7 is almost complete.

---
+++ 7. Tablice/script.js
@@ -0,0 +1,30 @@
+// Zadanie 1
+
+const arr = ['one', 'two', 13, "six"];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+// Zadanie 2
+
+// 1.
+
+console.log(arr[0]);
+console.log(arr[1]);
+
+// 2.
+
+console.log(arr[arr.length - 1]);
+
+// 3.
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+// 4.
+
+for (let i = 0; i < arr.length; i+=2) {
+    console.log(arr[i]);
+}
