cf33918e6c5db892d899b4b5072dbee07450e5cc
Jakub Twardosz <jtwardosz13@gmail.com>
2021-01-16 23:30:39 +0100

    Task 10 is almost done

---
+++ 11.DOM-wyszukiwanie/script.js
@@ -0,0 +1,26 @@
+// Zadanie 1
+
+let findListElements = document.querySelectorAll(".list");
+
+console.log(findListElements);
+
+// Zadanie 2 
+
+let findTagElements = function(tag){
+    return document.querySelectorAll(tag);
+}
+
+console.log(returnElements("body"));
+
+// Zadanie 3
+
+let findIdList = document.getElementById('list');
+
+console.log(findIdList);
+
+// Zadanie 4
+
+// const getElements = (selector) {
+//     return document.querySelectorAll(selector);
+// }
+
