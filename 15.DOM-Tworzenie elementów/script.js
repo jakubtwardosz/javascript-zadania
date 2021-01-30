ca66725c9b80f3ffe275bd129d74384a4cfc3fbd
Jakub Twardosz <jtwardosz13@gmail.com>
2021-01-30 22:43:08 +0100

    Task 15 is almost complete

---
+++ 15.DOM-Tworzenie elementów/script.js
@@ -0,0 +1,73 @@
+// Zadanie 1
+
+const newDiv = document.createElement('div');
+newDiv.innerText = 'To jest nowy element';
+
+document.body.appendChild(newDiv);
+
+// Zadanie 2
+
+const favouriteFruits = ['jabłko','pomarańcza','kiwi','banan','gruszka','truskawki','śliwki'];
+
+const ul = document.createElement("ul");
+favouriteFruits.forEach(v => {
+    const li = document.createElement("li");
+    li.innerText = v;
+    ul.appendChild(li);
+});
+document.body.appendChild(ul);
+
+// Zadanie 3
+
+document.body.addEventListener('click', function () {
+    const list = document.querySelectorAll("ul li");
+    list.forEach((element, i) => {
+        if (i % 2 === 0) element.remove();
+    });
+});
+
+// Zadanie 4
+
+const button = document.createElement('button');
+button.innerText = 'Kliknij tutaj';
+document.body.appendChild(button);
+
+button.addEventListener('click', function () {
+    button.remove();
+});
+
+// Zadanie 5
+
+const randomNumber = Math.floor((Math.random() * 10) + 1);
+
+for (let i = 0; i < randomNumber; i++) {
+    const div = document.createElement('div');
+    div.innerText = `to jest div ${i}`;
+    document.body.appendChild(div);
+}
+
+// Zadanie 6
+// Na podstawie obiektu stwórz struktórę w html
+// { div1: 'to jest div', span1: 'to jest span', div2: { div3: 'to jest div', }, span2: 'to jest span', }
+
+const obj = { div1: 'to jest div', span1: 'to jest span', div2: { div3: 'to jest div', }, span2: 'to jest span'};
+
+function objToHtml(obj, parent = document.body) {
+    Object.getOwnPropertyNames(obj).forEach(id => {
+        if (typeof (obj[id]) !== 'string') {
+            const innerParentTag = id.replace(/[0-9]/g, '');
+            const innerParent = document.createElement(innerParentTag);
+            innerParent.id = id;
+            parent.appendChild(innerParent);
+            objToHtml(obj[id], innerParent);
+            return;
+        }
+        const elTag = id.replace(/[0-9]/g, '');
+        const newEl = document.createElement(elTag);
+        newEl.innerText = obj[id];
+        newEl.id = id;
+        parent.appendChild(newEl);
+    })
+}
+
+objToHtml(obj);
