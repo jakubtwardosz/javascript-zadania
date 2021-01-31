1677c8c8162cf5c3d0e87e5d8bd977c43ec68128
Jakub Twardosz <jtwardosz13@gmail.com>
2021-01-31 12:25:13 +0100

    Task 15 still in progress

--- 15.DOM-Tworzenie elementów/script.js
+++ 15.DOM-Tworzenie elementów/script.js
@@ -47,8 +47,6 @@ for (let i = 0; i < randomNumber; i++) {
 }
 
 // Zadanie 6
-// Na podstawie obiektu stwórz struktórę w html
-// { div1: 'to jest div', span1: 'to jest span', div2: { div3: 'to jest div', }, span2: 'to jest span', }
 
 const obj = { div1: 'to jest div', span1: 'to jest span', div2: { div3: 'to jest div', }, span2: 'to jest span'};
 
@@ -71,3 +69,51 @@ function objToHtml(obj, parent = document.body) {
 }
 
 objToHtml(obj);
+
+// Zadanie 7
+
+const favouriteFruits1 = ['jabłko','pomarańcza','kiwi','banan','gruszka','truskawki','śliwki'];
+const ul1 = document.createElement("ul");
+const ul2 = document.createElement("ul");
+
+favouriteFruits1.forEach(v => {
+    const li = document.createElement("li");
+    li.innerText = v;
+    ul1.appendChild(li);
+});
+
+const listArr = [ul1, ul2];
+
+const buttons = [document.createElement("button"), document.createElement("button")];
+
+function checkButtonDisabled() {
+    listArr.forEach((ul, i) => {
+        if (ul.childElementCount <= 1){
+            buttons[i].disabled = true;
+        } else {
+            buttons[i].disabled = false;
+        }
+    })
+}
+
+listArr.forEach((ul, i) => {
+    buttons[i].innerText = 'Przenieś do drugiej listy';
+    buttons[i].addEventListener('click', () => {
+        const listItems = ul.querySelectorAll('li');
+        const childToTransfer = listItems[listItems.length - 1];
+        if (i === 0) {
+            ul2.insertBefore(childToTransfer, buttons[1]);
+        } else {
+            ul1.insertBefore(childToTransfer, buttons[0]);
+        }
+        checkButtonDisabled();
+    });
+    ul.appendChild(buttons[i]);
+    document.body.appendChild(ul);
+});
+
+checkButtonDisabled();
+
+// Zadanie 8
+
+// Stwórz formularz do którego będzie można wpisać jaki element user chce utworzyć, z jakim tekstem, z kolorem tekstu i ile razy ma być ten element powtórzony na stronie. Po kliknięciu na button "Utwórz" formularz powinien tworzyć taką strukturę.
